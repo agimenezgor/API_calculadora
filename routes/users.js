06a7ba8df39a03e0f4266af2e7da9c93fd9cfd94
agimenezgor <alvarogimenez88@gmail.com>
2021-01-27 11:07:51 +0100

    users routes preconfigured

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const UserController = require('../controllers/UserControllers');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+//router.get('/:email', UserController.getUser);
+//router.get('/', UserController.getAll);
+//router.post('/register', UserController.register);
+//router.put('/:email', UserController.update);
+//router.delete('/:email', UserController.delete);
+//router.post('/login', UserController.login);
 
-module.exports = router;
