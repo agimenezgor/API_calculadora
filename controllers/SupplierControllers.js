fbc7a9a7d4f8a4a24536dad4bec3dd8e20e5f7f7
agimenezgor <alvarogimenez88@gmail.com>
2021-02-03 19:01:12 +0100

    refactor register supplier

--- controllers/SupplierControllers.js
+++ controllers/SupplierControllers.js
@@ -3,26 +3,31 @@ const CONFIG = require('../config/config');
 const User = require('../models/User');
 const Supplier = require("../models/Supplier");
 
+async function findUser(token){
+    let userId = 0;
+    await jwt.verify(token, CONFIG.TOKEN_SECRET, async function(error, decoded){
+        if(error){
+            return res.status(403).send({message: 'Token incorrecto'});
+        }
+        const user = await User.findOne({email: decoded.email})
+        if(!user){
+            return res.status(401).send({message: 'No estás autorizado'});
+        }
+        userId = user._id;
+    });
+    return userId;
+}
 const SupplierController = {
     async register(req, res){
         try {
             // comprobar que el id del usuario existe
             let token = req.rawHeaders[1].split(' ')[1];
-            let userId = 0; 
-            await jwt.verify(token, CONFIG.TOKEN_SECRET, async function(error, decoded){
-                if(error){
-                    return res.status(403).send({message: 'Token incorrecto'});
-                }
-                const user = await User.findOne({email: decoded.email})
-                if(!user){
-                    return res.status(401).send({message: 'No estás autorizado'});
-                }
-                userId = user._id;
-                
-            })
+            let userId = await findUser(token);
+
             // Guardamos el id del usuario
             let supplierdata = Object(req.body);
-            supplierdata.user = userId;
+            /* supplierdata.user = userId; */
+            supplierdata.user = userId; 
 
             // Guardar en la base de datos
             const supplier = await Supplier.create(supplierdata);
@@ -39,6 +44,16 @@ const SupplierController = {
         } catch (error) {
             console.error(error);
             res.status(500).send({message: "There was a problem trying to get the supplier", error});
+        }
+    },
+
+    async getAll(req, res){
+        try {
+            const suppliers = await Supplier.find();
+            res.send(suppliers);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({message: "There was a problem trying to get all suppliers", error});
         }
     },
 }
