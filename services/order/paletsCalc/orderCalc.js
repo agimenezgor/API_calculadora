2d6fb8a113724499bae7385bfa678ae5a7d76826
agimenezgor <alvarogimenez88@gmail.com>
2021-02-21 17:16:14 +0100

    blue: paletsCalc: orderCalc

---
+++ services/order/paletsCalc/orderCalc.js
@@ -0,0 +1,33 @@
+function orderCalc (supplierConditioning, referenceArray, references) {
+    let orderArray = []
+    for(let i = 0; i < supplierConditioning; i++){
+        // buscamos mínimo días
+        let minDays = referenceArray[0].days;
+        let position = 0;
+        for(let j = 1; j < referenceArray.length; j++){
+            if(referenceArray[j].days < minDays){
+                minDays = referenceArray[j].days;
+                position = j;
+            }
+        }
+        // sumamos stock y recalculamos días
+        referenceArray[position].palets++;
+        let days = (referenceArray[position].palets * referenceArray[position].conditioning) / (referenceArray[position].sales / 30);
+        referenceArray[position].days = days.toFixed(2);
+        // guardamos la referencia en el array
+        if(!orderArray[position]){
+            let referenceObject = new Object();
+            referenceObject.name = referenceArray[position].name;
+            referenceObject.number = references[position].number;
+            referenceObject.conditioning = referenceArray[position].conditioning;
+            referenceObject.palets = 1;
+            orderArray[position] = referenceObject;
+        }
+        else{
+            orderArray[position].palets++;
+        }
+    }
+    
+    return orderArray;
+}
+module.exports = orderCalc;
--- services/order/paletsCalc/paletsCalc.js
+++ services/order/paletsCalc/paletsCalc.js
@@ -2,6 +2,7 @@ const Supplier = require("../../../models/Supplier");
 
 // Importando servicios
 const setReferencesArray = require("./setReferenceArray");
+const orderCalc = require("./orderCalc");
 
 async function paletsCalc(supplierId, references, palets){
     // Preparamos los datos necesarios antes de calcular
@@ -22,39 +23,10 @@ async function paletsCalc(supplierId, references, palets){
     if(supplierRemaining < maxPalets){
         supplierConditioning = minPalets;
     }
-
-    let orderArray = []
-    for(let i = 0; i < supplierConditioning; i++){
-        // buscamos mínimo días
-        let minDays = referenceArray[0].days;
-        let position = 0;
-        for(let j = 1; j < referenceArray.length; j++){
-            if(referenceArray[j].days < minDays){
-                minDays = referenceArray[j].days;
-                position = j;
-            }
-        }
-        // sumamos stock y recalculamos días
-        referenceArray[position].palets++;
-        let days = (referenceArray[position].palets * referenceArray[position].conditioning) / (referenceArray[position].sales / 30);
-        referenceArray[position].days = days.toFixed(2);
-        // guardamos la referencia en el array
-        if(!orderArray[position]){
-            let referenceObject = new Object();
-            referenceObject.name = referenceArray[position].name;
-            referenceObject.number = references[position].number;
-            referenceObject.conditioning = referenceArray[position].conditioning;
-            referenceObject.palets = 1;
-            orderArray[position] = referenceObject;
-        }
-        else{
-            orderArray[position].palets++;
-        }
-    }
     const order = Object();
         order.palets = palets;
         order.remaining = supplierRemaining - supplierConditioning;
-        order.orderArray = orderArray;
+        order.orderArray = orderCalc(supplierConditioning, referenceArray, references);
         order.message = message;
     return order;
 }
